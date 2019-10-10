72e905496062233ffae65549524044a2555d8b61
Zichuan.Yu <Zichuan.Yu@connext.com.cn>
2019-10-10 18:46:25 +0800

    increase

---
+++ ZeroStudy/ZeroTest.py
@@ -0,0 +1,54 @@
+
+## 7.2.3
+# class Geese:
+#     """大雁类"""
+#     def __init__(self):    #构造方法
+#         print('我是大雁类！')
+# wildGoose = Geese()   #创建大雁类的实例
+#
+# class Geese:
+#     """大雁类"""
+#     def __init__(self,beak,wing,claw):
+#         print('我是大雁类！我又以下特征：')
+#         print(beak)
+#         print(wing)
+#         print(claw)
+# beak_1 = '喙的基部较高，长度和头部的长度几乎相等'
+# wing_1 = '翅膀长而尖'
+# claw_1 = '爪子是蹼状的'
+# wildGoose = Geese(beak_1,wing_1,claw_1)
+
+#7.2.4
+
+# #Sample-1
+# class Geese:
+#     """大雁类"""
+#     def __init__(self,beak,wing,claw):
+#         print('我是大雁类！我有以下特征：')
+#         print(beak)
+#         print(wing)
+#         print(claw)
+#     def fly(self,state):
+#         print(state)
+# """************调用方法**********"""
+# beak_1 = '喙的基部较高，长度和头部的长度几乎相等'
+# wing_1 = '翅膀长而尖'
+# claw_1 = '爪子是蹼状的'
+# wildGoose = Geese(beak_1,wing_1,claw_1)
+# wildGoose.fly('我飞行的时候，一会排个人字，一会排个一字')
+
+# #Sample-1.2
+# class Geese:
+#     """大雁类"""
+#     neck = '脖子较长'
+#     wing = '振翅频率高'
+#     leg = '腿位于身体的中心支点，行走自如'
+#     def __init__(self):
+#         print('我是大雁类！我有以下特征：')
+#         print(Geese.neck)
+#         print(Geese.wing)
+#         print(Geese.leg)
+# geese = Geese()
+
+
+
