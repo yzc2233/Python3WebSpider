b0ab9163d8c7b50af5e230f3e090a95214febfe3
Zichuan.Yu <Zichuan.Yu@connext.com.cn>
2019-10-15 23:30:00 +0800

    update Lession12

--- ZeroStudy/Lession12/test12.py
+++ ZeroStudy/Lession12/test12.py
@@ -81,6 +81,147 @@ except ImportError as e:
 #     frame.Show()
 #     app.MainLoop()
 
+#
+# #使用BoxSizer实现简单的布局
+# import wx
+# class Myframe(wx.Frame):
+#     def __init__(self,parent,id):
+#         wx.Frame.__init__(self,parent,id,title='用户登录',size=(400,300))
+#         #创建面板
+#         panel = wx.Panel(self)
+#         self.title = wx.StaticText(panel,label='请输入用户名和密码')
+#         #添加容器，容器中控件按纵向排列
+#         vsizer = wx.BoxSizer(wx.VERTICAL)
+#         vsizer.Add(self.title,proportion=0,flag=wx.BOTTOM|wx.TOP|wx.ALIGN_CENTER,border=15)
+#         panel.SetSizer(vsizer)
+#
+# if __name__ == '__main__':
+#     app = wx.App()
+#     frame = Myframe(parent=None,id=-1)
+#     frame.Show()
+#     app.MainLoop()
+
+
+#
+# #使用BoxSizer设置登录页面布局
+# import wx
+# class Myframe(wx.Frame):
+#     def __init__(self,parent,id):
+#         wx.Frame.__init__(self,parent,id,title='用户登录',size=(400,300))
+#         panel = wx.Panel(self)
+#
+#         #创建确定和取消按钮
+#         self.bt_confirm = wx.Button(panel,label='确定')
+#         self.bt_cancel = wx.Button(panel,label='取消')
+#
+#         #创建文本，左对齐
+#         self.title = wx.StaticText(panel,label='请输入用户和密码')
+#         self.label_user = wx.StaticText(panel,label='用户名：')
+#         self.text_user = wx.TextCtrl(panel,style=wx.TE_LEFT)
+#         self.label_pwd = wx.StaticText(panel,label='密   码：')
+#         self.text_pwd = wx.TextCtrl(panel,style=wx.TE_PASSWORD)
+#
+#         #添加容器，容器中的控件横向排列
+#         hsizer_user = wx.BoxSizer(wx.HORIZONTAL)
+#         hsizer_user.Add(self.label_user,proportion=0,flag=wx.ALL,border=5)
+#         hsizer_user.Add(self.text_user,proportion=1,flag=wx.ALL,border=5)
+#
+#         hsizer_pwd = wx.BoxSizer(wx.HORIZONTAL)
+#         hsizer_pwd.Add(self.label_pwd,proportion=0,flag=wx.ALL,border=5)
+#         hsizer_pwd.Add(self.text_pwd,proportion=1,flag=wx.ALL,border=5)
+#
+#         hsizer_button = wx.BoxSizer(wx.HORIZONTAL)
+#         hsizer_button.Add(self.bt_confirm,proportion=0,flag=wx.ALIGN_CENTER,border=5)
+#         hsizer_button.Add(self.bt_cancel,proportion=0,flag=wx.ALIGN_CENTER,border=5)
+#
+#         #添加容器，容器中的控件纵向排列
+#         vsizer_all = wx.BoxSizer(wx.VERTICAL)
+#         vsizer_all.Add(self.title,proportion=0,flag=wx.BOTTOM|wx.TOP|wx.ALIGN_CENTER,border=15)
+#         vsizer_all.Add(hsizer_user,proportion=0,flag=wx.EXPAND|wx.LEFT|wx.RIGHT,border=45)
+#         vsizer_all.Add(hsizer_pwd,proportion=0,flag=wx.EXPAND|wx.LEFT|wx.RIGHT,border=45)
+#         vsizer_all.Add(hsizer_button,proportion=0,flag=wx.ALIGN_CENTER|wx.TOP,border=15)
+#
+#         panel.SetSizer(vsizer_all)
+#
+# if __name__ == '__main__':
+#     app = wx.App()
+#     frame = Myframe(parent=None,id=-1)
+#     frame.Show()
+#     app.MainLoop()
+
+
+#使用事件判断用户登录
+import wx
+class Myframe(wx.Frame):
+    def __init__(self,parent,id):
+        wx.Frame.__init__(self,parent,id,'用户登录',size=(400,300))
+
+        panel = wx.Panel(self)
+
+        self.title = wx.StaticText(panel,label='请输入用户名和密码')
+
+
+
+        self.bt_confirme = wx.Button(panel,label='确定')
+        self.bt_confirme.Bind(wx.EVT_BUTTON,self.OnclickSubmit)
+        self.bt_cancel =  wx.Button(panel,label='取消')
+        self.bt_cancel.Bind(wx.EVT_BUTTON,self.OnclickCancel)
+
+        self.label_user = wx.StaticText(panel,label='用户名：')
+        self.text_user = wx.TextCtrl(panel,style=wx.TE_LEFT)
+        self.label_pwd = wx.StaticText(panel,label='密   码：')
+        self.text_pwd = wx.TextCtrl(panel,style=wx.TE_PASSWORD)
+
+        hsizer_user = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer_user.Add(self.label_user,proportion=0,flag=wx.ALL,border=5)
+        hsizer_user.Add(self.text_user,proportion=1,flag=wx.ALL,border=5)
+
+        hsizer_pwd = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer_pwd.Add(self.label_pwd,proportion=0,flag=wx.ALL,border=5)
+        hsizer_pwd.Add(self.text_pwd,proportion=1,flag=wx.ALL,border=5)
+
+        hsizer_bt = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer_bt.Add(self.bt_confirme,proportion=0,flag=wx.ALL,border=5)
+        hsizer_bt.Add(self.bt_cancel,proportion=1,flag=wx.ALL,border=5)
+
+        hsizer_all = wx.BoxSizer(wx.VERTICAL)
+        hsizer_all.Add(self.title,proportion=0,flag=wx.BOTTOM|wx.TOP|wx.ALIGN_CENTER,border=15)
+        hsizer_all.Add(hsizer_user,proportion=0,flag=wx.EXPAND| wx.LEFT | wx.RIGHT,border=5)
+        hsizer_all.Add(hsizer_pwd,proportion=0,flag=wx.EXPAND| wx.LEFT | wx.RIGHT,border=5)
+        hsizer_all.Add(hsizer_bt,proportion=0,flag=wx.ALIGN_CENTER,border=5)
+
+        panel.SetSizer(hsizer_all)
+
+    def OnclickSubmit(self,event):
+        message = ''
+        #获取输入的用户名和密码
+        usename = self.text_user.GetValue()
+        pwd = self.text_pwd.GetValue()
+        if usename == '' or pwd == '':
+            message = '用户名或者密码不能为空'
+        elif usename == 'mr' and pwd == 'mrsoft':
+            message = '登陆成功'
+        else:
+            message = '用户名和密码不匹配'
+        wx.MessageBox(message)
+
+
+
+    def OnclickCancel(self,event):
+        #清空用户名和密码
+        self.text_user.SetValue('')
+        self.text_pwd.SetValue('')
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = Myframe(parent=None,id=-1)
+    frame.Show()
+    app.MainLoop()
+
+
+
+
 
 
 
