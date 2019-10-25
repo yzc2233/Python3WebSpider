b1935ed179b1489c86448dc6a532f0412dfad6df
Zichuan.Yu <Zichuan.Yu@connext.com.cn>
2019-10-25 17:57:30 +0800

    update

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 url = 'http://10.157.24.142:10016/swagger-ui.html'
-url2 = 'http://10.157.24.142:10016/swagger-resources/configuration/security'
 headers = {
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
     'Accept-Encoding':'gzip, deflate',
@@ -19,24 +18,36 @@ headers = {
     'Upgrade-Insecure-Requests':'1',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.17 Safari/537.36'
 }
-headers2 = {
-    'Accept':'application/json, text/javascript, */*; q=0.01',
-    'Accept-Encoding':'gzip, deflate',
-    'Accept-Language':'zh-CN,zh;q=0.9',
-    'Connection':'keep-alive',
-    'Host':'10.157.24.142:10016',
-    'Referer':'http://10.157.24.142:10016/swagger-ui.html',
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.17 Safari/537.36',
-    'X-Requested-With':'XMLHttpRequest'
-}
 
-browser = webdriver.Chrome()
-
-# req = requests.get(url,headers=headers)
-# req2 = requests.get(url2,headers=headers2)
-# req3 = requests.get(url,headers=headers)
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+browser = webdriver.Chrome(chrome_options=option)
 browser.get(url)
-wait = WebDriverWait(browser,10)
+wait = WebDriverWait(browser,3)
 wait.until(EC.presence_of_element_located((By.ID,'resource_behavior45post45controller')))
+data = browser.page_source
+# print(data)
+
+reqtype = browser.find_element_by_xpath('/html/body/div[3]/div[2]/ul/li[1]/ul/li[1]/ul/li/div[1]/h3/span[1]/a')
+print(str(reqtype.text))
+
+
+
+
+
+
+
 browser.close()
-print(browser.page_source)
+
+
+
+
+
+
+
+
+
+
+
+
+
