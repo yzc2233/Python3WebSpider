f006f6db7f759b64071b8cc7f9fee91001fc138f
Zichuan.Yu <Zichuan.Yu@connext.com.cn>
2019-10-28 19:44:33 +0800

    update

--- ZeroStudy/Lession14/CheckTicket/query_request.py
+++ ZeroStudy/Lession14/CheckTicket/query_request.py
@@ -1,25 +1,33 @@
 import requests
 from get_station import *
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 
 data = []  #保存整理好的车次信息
 type_data = []  #保存分类后车次信息
 
-def query(data,form_station,to_station):
+def query(date,form_station,to_station):
     data.clear() #清空数据
     type_data.clear() #清空车次分类保存的数据
+    headers = {'If-Modified-Since':'0','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.17 Safari/537.36',
+               'X-Requested-With':'XMLHttpRequest','Cookie':'JSESSIONID=537D8878010B0E64F0518BB688798503; BIGipServerotn=837812746.24610.0000; BIGipServerpool_passport=250413578.50215.0000; RAIL_EXPIRATION=1572551466304; RAIL_DEVICEID=C3aUTdBb5by5Pzjau7aeNNdaZo4FJUs_wARhs4wPuihY24NKMn_E8qRYnC8lj2_il6okSYH7Yu-wAKkJAbU38oudE-awkJ49agPipIJmu5rMu2pEQIPqtrcjiroqUowyUhhYH-7qLF_GrIX2CHnm10EY1xrVnd4j; route=495c805987d0f5c8c84b14f60212447d; _jc_save_fromStation=%u4E0A%u6D77%2CSHH; _jc_save_toStation=%u5317%u4EAC%2CBJP; _jc_save_fromDate=2019-10-28; _jc_save_toDate=2019-10-28; _jc_save_wfdc_flag=dc'}
 
-    url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(data,form_station,to_station)
-    response = requests.get(url)
+    url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(date,form_station,to_station)
+
+    response = requests.get(url,headers=headers)
     result = response.json()
     result = result['data']['result']
 
-    if isStation():
+    if isStations() == True:
         stations = eval(read()) #读取所有车站并转换为dic类型
         if len(result) != 0:
             for i in result:
                 tmp_list = i.split('|')
                 from_station = list(stations.keys())[list(stations.values()).index(tmp_list[6])]
-                to_station = list(stations.keys())[list(stations.values()).index(tmp_list[6])]
+                to_station = list(stations.keys())[list(stations.values()).index(tmp_list[7])]
                 seat = [
                     tmp_list[3],from_station,to_station,tmp_list[8],tmp_list[9],tmp_list[10],
                     tmp_list[32],tmp_list[31],tmp_list[30],tmp_list[21],tmp_list[23],tmp_list[33],
@@ -33,8 +41,9 @@ def query(data,form_station,to_station):
                         s = s
                     newseat.append(s)
                 data.append(newseat)
-            return data
+    return data
 
+#获取高铁信息
 def g_vehicle():
     if len(data) != 0:
         for g in data:
@@ -50,6 +59,7 @@ def r_g_vehicle():
             if i:
                 type_data.remove(g)
 
+#获取动车信息
 def d_vehicle():
     if len(data) != 0:
         for d in data:
@@ -57,7 +67,7 @@ def d_vehicle():
             if i:
                 type_data.append(d)
 
-
+#动车信息移除
 def r_d_vehicle():
     if len(data) != 0:
         for d in data:
--- ZeroStudy/Lession14/CheckTicket/window.py
+++ ZeroStudy/Lession14/CheckTicket/window.py
@@ -7,8 +7,13 @@
 # WARNING! All changes made in this file will be lost!
 
 import sys
+import time
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QPalette,QPixmap,QColor
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from query_request import *
+from get_station import *
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -38,27 +43,6 @@ class Ui_MainWindow(object):
         palette.setBrush(QPalette.Background,QtGui.QBrush(QtGui.QPixmap('img/bg2.png')))
         self.widget_query.setPalette(palette) #为控件设置对应的调色板即可
 
-        #车次选项
-        self.widget_checkBox = QtWidgets.QWidget(self.centralwidget)
-        self.widget_checkBox.setGeometry(QtCore.QRect(0, 220, 960, 80))
-        self.widget_checkBox.setObjectName("widget")
-        self.widget_checkBox.setAutoFillBackground(True)
-        palette.setBrush(QPalette.Background,QtGui.QBrush(QtGui.QPixmap('img/bg3.png')))
-        self.widget_checkBox.setPalette(palette)
-
-        #分类图片
-        self.label_train_img = QtWidgets.QLabel(self.centralwidget)
-        self.label_train_img.setGeometry(QtCore.QRect(0, 256, 960, 60))
-        self.label_train_img.setObjectName("label_train_img")
-        train_img = QPixmap('img/bg4.png')
-        self.label_train_img.setPixmap(train_img)
-
-        #显示信息表格
-        self.tableView = QtWidgets.QTableView(self.centralwidget)
-        self.tableView.setGeometry(QtCore.QRect(0, 320, 960, 446))
-        self.tableView.setObjectName("tableView")
-
-
         self.label = QtWidgets.QLabel(self.widget_query)
         self.label.setGeometry(QtCore.QRect(30, 30, 54, 12))
         self.label.setObjectName("label")
@@ -82,6 +66,13 @@ class Ui_MainWindow(object):
         self.textEdit_3.setGeometry(QtCore.QRect(460, 20, 104, 31))
         self.textEdit_3.setObjectName("textEdit_3")
 
+        #车次选项
+        self.widget_checkBox = QtWidgets.QWidget(self.centralwidget)
+        self.widget_checkBox.setGeometry(QtCore.QRect(0, 220, 960, 80))
+        self.widget_checkBox.setObjectName("widget")
+        self.widget_checkBox.setAutoFillBackground(True)
+        palette.setBrush(QPalette.Background,QtGui.QBrush(QtGui.QPixmap('img/bg3.png')))
+        self.widget_checkBox.setPalette(palette)
 
         self.checkBox_D = QtWidgets.QCheckBox(self.widget_checkBox)
         self.checkBox_D.setGeometry(QtCore.QRect(258, 9, 63, 17))
@@ -102,17 +93,34 @@ class Ui_MainWindow(object):
         self.label_type.setGeometry(QtCore.QRect(30, 9, 65, 16))
         self.label_type.setObjectName("label_type")
 
+        #分类图片
+        self.label_train_img = QtWidgets.QLabel(self.centralwidget)
+        self.label_train_img.setGeometry(QtCore.QRect(0, 256, 960, 60))
+        self.label_train_img.setObjectName("label_train_img")
+        train_img = QPixmap('img/bg4.png')
+        self.label_train_img.setPixmap(train_img)
+
+        #显示信息表格
+        self.tableView = QtWidgets.QTableView(self.centralwidget)
+        self.tableView.setGeometry(QtCore.QRect(0, 320, 960, 440))
+        self.tableView.setObjectName("tableView")
+        self.model = QStandardItemModel() #创建存储数据的模式
+        #根据空间自动改变列宽度并且不可修改列宽度
+        self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        #设置表头不可见
+        self.tableView.horizontalHeader().setVisible(False)
+        #纵向表头不可见
+        self.tableView.verticalHeader().setVisible(False)
+        #设置表格内容文字大小
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.tableView.setFont(font)
+        #设置表格内容不可编辑
+        self.tableView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        #垂直滚动条始终开启
+        self.tableView.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 848, 23))
-        self.menubar.setObjectName("menubar")
-        self.menu = QtWidgets.QMenu(self.menubar)
-        self.menu.setObjectName("menu")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-        self.menubar.addAction(self.menu.menuAction())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -133,6 +141,143 @@ class Ui_MainWindow(object):
         self.label_3.setText(_translate("MainWindow", "出发日："))
         self.pushButton.setText(_translate("MainWindow", "查询"))
 
+        self.textEdit_3.setText(get_time())#触发日期显示当天日期
+        self.pushButton.clicked.connect(self.on_click)#查询按钮指定单击事件的方法
+        self.checkBox_G.stateChanged.connect(self.change_G)#高铁选中与取消事件
+        self.checkBox_D.stateChanged.connect(self.change_D)#动车选中与取消事件
+        self.checkBox_Z.stateChanged.connect(self.change_Z)#直达选中与取消事件
+        self.checkBox_T.stateChanged.connect(self.change_T)#特快选中与取消事件
+        self.checkBox_K.stateChanged.connect(self.change_K)#快车选中与取消事件
+
+    #高铁复选框事件处理
+    def change_G(self,state):
+        #选中将高铁信息添加到最后要显示的数据中
+        if state == QtCore.Qt.Checked:
+            g_vehicle()
+            #通过表格显示该车型数据
+            self.displayTable(len(type_data),16,type_data)
+        else:
+            #取消选中状态将移除该数据
+            r_g_vehicle()
+            self.displayTable(len(type_data),16,type_data)
+
+    def change_D(self,state):
+        #选中将动车信息添加到最后要显示的数据中
+        if state == QtCore.Qt.Checked:
+            d_vehicle()
+            #通过表格显示该车型数据
+            self.displayTable(len(type_data),16,type_data)
+        else:
+            #取消选中状态将移除该数据
+            r_d_vehicle()
+            self.displayTable(len(type_data),16,type_data)
+
+    def change_Z(self,state):
+        #选中将直达车信息添加到最后要显示的数据中
+        if state == QtCore.Qt.Checked:
+            z_vehicle()
+            #通过表格显示该车型数据
+            self.displayTable(len(type_data),16,type_data)
+        else:
+            #取消选中状态将移除该数据
+            r_z_vehicle()
+            self.displayTable(len(type_data),16,type_data)
+
+    def change_T(self,state):
+        #选中将特快车信息添加到最后要显示的数据中
+        if state == QtCore.Qt.Checked:
+            t_vehicle()
+            #通过表格显示该车型数据
+            self.displayTable(len(type_data),16,type_data)
+        else:
+            #取消选中状态将移除该数据
+            r_t_vehicle()
+            self.displayTable(len(type_data),16,type_data)
+
+    def change_K(self,state):
+        #选中将快速车信息添加到最后要显示的数据中
+        if state == QtCore.Qt.Checked:
+            k_vehicle()
+            #通过表格显示该车型数据
+            self.displayTable(len(type_data),16,type_data)
+        else:
+            #取消选中状态将移除该数据
+            r_k_vehicle()
+            self.displayTable(len(type_data),16,type_data)
+
+    #所有车次分类复选框取消勾选
+    def checkBox_default(self):
+        self.checkBox_G.setChecked(False)
+        self.checkBox_D.setChecked(False)
+        self.checkBox_Z.setChecked(False)
+        self.checkBox_T.setChecked(False)
+        self.checkBox_K.setChecked(False)
+
+    #显示主窗体非法操作的消息提示框
+    #title为提示框标题文字，message为提示信息
+    def messageDialog(self,title,message):
+        msg_box = QMessageBox(QMessageBox.Warning,title,message)
+        msg_box.exec_()
+
+    #展示车次信息的表格
+    #train为共有多少列车，该参数作为表格的行
+    #info为该车次的具体信息，例如有座、无座、卧铺等，该参数作为表格的列
+    def displayTable(self,train,info,data):
+        self.model.clear()
+        for row in range(train):
+            for column in range(info):
+                #添加表格内容
+                item = QStandardItem(data[row][column])
+                #向表格存储模式中添加表格具体信息
+                self.model.setItem(row,column,item)
+        #设置表格存储数据的模式
+        self.tableView.setModel(self.model)
+
+    #查询按钮的单击事件
+    def on_click(self):
+        get_from = self.textEdit.toPlainText()#获取出发地
+        get_to = self.textEdit_2.toPlainText()#获取到达地
+        get_date = self.textEdit_3.toPlainText()#获取出发时间
+        #判断车站文件是否存在
+        if isStations() == True:
+            stations = eval(read()) #读取所有车站并转换为dic类型
+            #判断所有参数是否为空，以及出发地、目的地、出发日期
+            cc = (is_valid_date(get_date))
+            bb = (get_from in stations)
+            aa = (get_to in stations)
+            if get_from != '' and get_to != '' and get_date !='':
+                #判断输入的车站名称是否存在，以及时间格式是否正确
+                if get_from in stations and get_to in stations and is_valid_date(get_date):
+                    #获取输入的日期是当前年初到现在一共过了多少天
+                    inputYearDay = time.strptime(get_date,'%Y-%m-%d').tm_yday
+                    #获取系统当前日期是当前年初到现在一共过了多少天
+                    yearTody = time.localtime(time.time()).tm_yday
+                    #计算时间差，也就是输入的日期减掉系统当前的日期
+                    timeDifference = inputYearDay - yearTody
+                    #判断时间差为0时证明是查询当天的查票
+                    #以及29天以后的车票，12306要求只能查询30天以内的车票
+                    if timeDifference >= 0 and timeDifference <= 28:
+                        #在所有车站文件中找到对应的参数
+                        from_station  =  stations[get_from]#出发地
+                        to_station = stations[get_to]#目的地
+                        #发起查询请求，并获取返回的信息
+                        data = query(get_date,from_station,to_station)
+                        self.checkBox_default() #调用取消勾选所有车次分类复选框
+                        if len(data) != 0:#判断返回数据是否为空
+                            #如果不为空数据就将车票信息显示在表格
+                            self.displayTable(len(data),16,data)
+                        else:
+                            self.messageDialog('警告','没有返回的网络数据！')
+                    else:
+                        self.messageDialog('警告','超出查询日期的范围内','不可查询昨天的车票信息，以及29天以后的车票信息！')
+                else:
+                    self.messageDialog('警告','输入的站名不存在，或日期格式不正确！')
+            else:
+                self.messageDialog('警告','请填写车站名称！')
+        else:
+            self.messageDialog('警告','未下载车站查询文件！')
+
+
 def show_MainWindow():
     app = QtWidgets.QApplication(sys.argv) #实例化QApplication类，作为GUI主程序入口
     MainWindow = QtWidgets.QMainWindow() #创建MainWindow类
@@ -141,5 +286,27 @@ def show_MainWindow():
     MainWindow.show() #显示窗体
     sys.exit(app.exec_()) #当窗体创建完成，需要结束主循环过程
 
+#获取系统当前时间并转换请求数据所需的格式
+def get_time():
+    #获取当前时间的时间戳
+    now = int(time.time())
+    #转换为其他日期格式，如：“%Y-%m-%d %H:%M:%S”
+    timeStruct = time.localtime(now)
+    strTime = time.strftime("%Y-%m-%d",timeStruct)
+    return strTime
+
+def is_valid_date(str):
+    """判断是否是一个有效的日期字符串"""
+    try:
+        time.strptime(str,"%Y-%m-%d")
+        return True
+    except:
+        return False
+
+
 if __name__ == '__main__':
-    show_MainWindow()
+    if isStations() == False:
+        getStation()
+        show_MainWindow()
+    else:
+        show_MainWindow()
