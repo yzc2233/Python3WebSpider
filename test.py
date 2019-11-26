dd460fa9edd4fa2bc69d7594969abfe4f2d136b4
Zichuan.Yu <Zichuan.Yu@connext.com.cn>
2019-11-26 23:32:00 +0800

    2019-1-26 update

--- test.py
+++ test.py
@@ -7,31 +7,24 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-url = 'http://10.157.24.142:10016/swagger-ui.html'
-headers = {
-    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-    'Accept-Encoding':'gzip, deflate',
-    'Accept-Language':'zh-CN,zh;q=0.9',
-    'Cache-Control':'max-age=0',
-    'Connection':'keep-alive',
-    'Host':'10.157.24.142:10016',
-    'Upgrade-Insecure-Requests':'1',
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.17 Safari/537.36'
-}
+url = 'http://10.157.24.132:10016/swagger-ui.html'
 
-option = webdriver.ChromeOptions()
-option.add_argument('headless')
-browser = webdriver.Chrome(chrome_options=option)
+option = webdriver.firefox.options.Options()
+option.add_argument('-headless')#无头模式
+browser = webdriver.Firefox(firefox_options=option)
 browser.get(url)
-wait = WebDriverWait(browser,3)
-wait.until(EC.presence_of_element_located((By.ID,'resource_behavior45post45controller')))
+wait = WebDriverWait(browser,10)
+wait.until(EC.presence_of_element_located((By.ID,'resources')))
 data = browser.page_source
 # print(data)
+datahtml = BeautifulSoup(data,'html.parser')
+# ApiControlList = datahtml.find_all(class_='endpoints',attrs={'style':'display: block;'})
+ApiControlList = datahtml.select("ul>li>ul>li>ul>li .heading")
+print(ApiControlList)
 
-reqtype = browser.find_element_by_xpath('/html/body/div[3]/div[2]/ul/li[1]/ul/li[1]/ul/li/div[1]/h3/span[1]/a')
-print(str(reqtype.text))
-
-
+# for ApiControl in ApiControlList:
+#     ApiList = ApiControl.find_all(name='div',class_='heading')
+#     print(ApiList)
 
 
 
