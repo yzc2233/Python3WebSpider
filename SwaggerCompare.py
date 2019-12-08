0fe648b7da6bface30324ec81cca73ec92936c86
Zichuan.Yu <Zichuan.Yu@connext.com.cn>
2019-12-08 18:51:08 +0800

    完成对比

--- SwaggerCompare.py
+++ SwaggerCompare.py
@@ -2,52 +2,72 @@ import os
 import json
 import sys
 from myFunction import *
-from compareTwoFileAndSave import *
 
-serviceList = ['ALL service','COMMUNITY-CORE-SERVICE','CRM-SERVICE','FINANCE-SERVICE','INV-SERVICE',
-               'KOL-SERVICE','MARKETING-ACTIVITY-SERVICE','MESSAGES-SERVICE','MP-CMS-SERVICE',
-               'MYACCOUNT-PORTAL-SERVICE','MYACCOUNT-SERVICE','OFFLINE-SHOP-SERVICE','OMNI-CRMHUB-SERVICE',
-              # 'OMNI-MEMBER-MSG-SERVICE','OMNI-ORDERCENTER-SERVICE',
-               'ORDER-SERVICE','PIM-BACKEND',
-               'PRODUCT-SERVICE','PROMOTION-SERVICE','SEARCH-SERVICE',
-               'SHOP-CART-SERVICE','SOA-MANAGEMENT','OMS-SERVICE','WCS-SERVICE','WECHATCENTER-SERVICE']
-print('命令示例：python SwaggerCompare.py 1')
-print('示例中的1是service序号，以下为各service序号(0代表以下所有services)')
-for service in serviceList:
-    print(serviceList.index(service),':',service)
+#
+# serviceList = ['ALL service','COMMUNITY-CORE-SERVICE','CRM-SERVICE','FINANCE-SERVICE','INV-SERVICE',
+#                'KOL-SERVICE','MARKETING-ACTIVITY-SERVICE','MESSAGES-SERVICE','MP-CMS-SERVICE',
+#                'MYACCOUNT-PORTAL-SERVICE','MYACCOUNT-SERVICE','OFFLINE-SHOP-SERVICE','OMNI-CRMHUB-SERVICE',
+#               # 'OMNI-MEMBER-MSG-SERVICE','OMNI-ORDERCENTER-SERVICE',
+#                'ORDER-SERVICE','PIM-BACKEND',
+#                'PRODUCT-SERVICE','PROMOTION-SERVICE','SEARCH-SERVICE',
+#                'SHOP-CART-SERVICE','SOA-MANAGEMENT','OMS-SERVICE','WCS-SERVICE','WECHATCENTER-SERVICE']
+# print('命令示例：python SwaggerCompare.py 1')
+# print('示例中的1是service序号，以下为各service序号(0代表以下所有services)')
+# for service in serviceList:
+#     print(serviceList.index(service),':',service)
 
+DealServicesList = ['MYACCOUNT-PORTAL-SERVICE']
+env = 'qa2'
 
-def main(realservicelist):
-    for service in realservicelist:
-        #检查service文件夹是否存在，不存在则创建
-        APIJson_Dir,APICompare_Dir,service_isExist = f.createServiceDir(service)
+def main(DealServicesList):
+    envFilePathList = []#生成的env文件路径列表
+    prdFilePathList = []#生成的prd文件路径列表
+    compareFilePathList = []#对比结果文件路径列表
+    isNew = True
+    for service in DealServicesList:
+        # 创建相应文件夹
+        serviceFileDir,compareServiceFileDir = createServiceDir(service,env)
+        #从swagger页面获取env环境API数据
+        envApiList = getApiList(service,env)
+        envData = {service:envApiList}
+        #从swagger页面获取pr环境API数据
+        prdApiList = getApiList(service,'prd')
+        prdData = {service:prdApiList}
+        #保存env环境API数据
+        envFileDir = saveAPIJosn(serviceFileDir,env+'-'+service,envData)
+        envFilePathList.append(envFileDir)
+        #保存prd环境API数据
+        prdFileDir = saveAPIJosn(serviceFileDir,'prd'+'-'+service,prdData)
+        prdFilePathList.append(prdFileDir)
+        #对比两个文件
+        prdLen,envLen,addCount,addList,deleteCount,deleteList = getAddAndDeleteCount(prdAPIList=prdApiList,envAPIList=envApiList)
+        compare = showCompareResults(service,prdLen,envLen,addCount,addList,deleteCount,deleteList)
+        #保存service对比结果
+        compareFileDir = saveAPIJosn(compareServiceFileDir,'compare'+'-'+service,compare)
+        compareFilePathList.append(compareFileDir)
+        #将service对比结果保存进对比汇总文件中
+        compareFilePath = saveAllcompareresult(compareFileDir,compare,isNew=isNew)
+        isNew = False
+    print('对比结果文件路径：' + compareFilePath)
 
-        ##拉取最新的Swagger接口信息并保存
-        if service_isExist:
-            APIJson = getAPIJosn(service)
-            saveAPIJosn(APIJson_Dir,APIJson)
-        else:
-            APIJson1 = getAPIJosn(service)
-            saveAPIJosn(APIJson_Dir,APIJson1)
-            APIJson2 = getAPIJosn(service)
-            saveAPIJosn(APIJson_Dir,APIJson2)
-
-        #获取当前service最新的Json文件和上一文件
-        oldFilePath,newFilePath = getOldAndNewFilePath(APIJson_Dir)
-
-        #对比两个文件保存结果并展示
-        CompareTwoFileAndSave(oldFilePath,newFilePath,APICompare_Dir)
-
+    #
+    # for service in realservicelist:
+    #
+    #
+    #
+    #     #对比两个文件保存结果并展示
+    #     CompareTwoFileAndSave(oldFilePath,newFilePath,APICompare_Dir)
+#
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        serviceIndex = int(input('请输入service序号：'))
-    else:
-        serviceIndex = int(sys.argv[1])
-    # serviceIndex = 19
-    realservicelist = []
-    if serviceIndex != 0:
-        realservicelist.append(serviceList[serviceIndex])
-    else:
-        realservicelist = serviceList[1:]
-    main(realservicelist)
+#     if len(sys.argv) == 1:
+#         serviceIndex = int(input('请输入service序号：'))
+#     else:
+#         serviceIndex = int(sys.argv[1])
+#     # serviceIndex = 19
+#     realservicelist = []
+#     if serviceIndex != 0:
+#         realservicelist.append(serviceList[serviceIndex])
+#     else:
+#         realservicelist = serviceList[1:]
+    main(DealServicesList)
 
--- myFunction.py
+++ myFunction.py
@@ -4,215 +4,150 @@ import time
 import requests
 import getIP
 import cmdprintcolor
+import datetime
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from collections import OrderedDict
+
+curDir = os.getcwd() #获取当前目录路径
+TodayDate = str(datetime.date.today())   #获取当前日期
 
 #检查service文件夹是否存在，不存在则创建
-def createServiceDir(service):
-    curDir = os.getcwd()
-    servicepath = os.path.join(curDir,'Services',service)
-    isExist = os.path.exists(servicepath)
-    service_APIJson_Dir = os.path.join(servicepath,'APIJSON')
-    service_APICompare_Dir = os.path.join(servicepath,'APICompare')
-    if not isExist:
-        os.makedirs(service_APIJson_Dir)
-        print(service_APIJson_Dir,'创建成功')
-        os.makedirs(service_APICompare_Dir)
-        print(service_APICompare_Dir,'创建成功')
-    return service_APIJson_Dir,service_APICompare_Dir,isExist
+def createServiceDir(service,env='stage'):
+    serviceFileDir = os.path.join(curDir,TodayDate,env,'ServicesAPI') #API数据文件目录
+    compareServiceFileDir = os.path.join(curDir,TodayDate,env,'CompareResults') #对比结果文件目录
+    #判断目录是否已存在
+    isExist_data = os.path.exists(serviceFileDir)
+    #不存在则创建
+    if not isExist_data:
+        os.makedirs(serviceFileDir)
+    isExist_compare = os.path.exists(compareServiceFileDir)
+    if not isExist_compare:
+        os.makedirs(compareServiceFileDir)
+    return serviceFileDir,compareServiceFileDir
 
-#拉取最新的Swagger接口信息
-def getAPIJosn(service):
-    service_ip = getIP.getIP(service=service)
-    service_swaggerUrl = service_ip+'/v2/api-docs'
-    res = requests.get(service_swaggerUrl)
-    res_json = json.loads(res.text)
-    return res_json
+#获取service对应的Html路径
+def getSwaggerUrl(service,env='stage'):
+    ip = getIP.getIP(service=service,env=env)
+    url = ip + '/swagger-ui.html'
+    return url
+
+#从swagger页面获取API数据
+def getApiList(service,env='stage'):
+    #获取service对应的Html路径
+    url = getSwaggerUrl(service,env)
+    #设置FireFox驱动器路径
+    DriverFlieDir = os.path.join(curDir,'geckodriver')
+    #设置浏览器
+    option = webdriver.firefox.options.Options()
+    option.add_argument('-headless')#无头模式，后台打开浏览器
+    browser = webdriver.Firefox(executable_path=DriverFlieDir,firefox_options=option)
+    browser.get(url)
+    wait = WebDriverWait(browser,10)
+    wait.until(EC.presence_of_element_located((By.ID,'resources')))
+    data = browser.page_source
+    datahtml = BeautifulSoup(data,'lxml')
+    ApiHtmlList = datahtml.select("ul > li > ul > li > ul > li .heading") #Api元素列表
+    ApiList = []
+    for ApiHtml in ApiHtmlList:
+        Api = OrderedDict()
+        Api_type = ApiHtml.h3.span.a.string
+        Api_path = ApiHtml.find(class_='path').a.string
+        Api_Note = ApiHtml.ul.li.a.span.p.string
+        Api['Type'] = Api_type
+        Api['Path'] = Api_path
+        Api['Description'] = Api_Note
+        ApiList.append(Api)
+    browser.close()
+    return ApiList
 
 #保存最新的Swagger接口信息
-def saveAPIJosn(path,data):
-    filetime = time.strftime('%Y%m%d',time.localtime())
-    filename = filetime+'.json'
-    filepath = os.path.join(path,filename)
-    if len(os.listdir(path)) == 1:
-        filename = filetime+'_new'+'.json'
-        filepath = os.path.join(path,filename)
+def saveAPIJosn(path,name,data):
+    filepath = os.path.join(path,name+'.json')
     with open(filepath,'w',encoding='utf8') as file:
-        json.dump(data,file,ensure_ascii=False,indent=4,sort_keys=True)
+        json.dump(data,file,ensure_ascii=False,indent=4)
     return filepath
 
-#获取最近两个Swagger接口信息文件路径
-def getOldAndNewFilePath(path):
-    DirFileList = os.listdir(path)
-    oldFileName = DirFileList[-2]
-    oldFilePath = os.path.join(path,oldFileName)
-    newFileName = DirFileList[-1]
-    newFilePath = os.path.join(path,newFileName)
-    return oldFilePath,newFilePath
-
-
-def getoldAndNewFiledata(oldFilePath,newFilePath):
-    with open(oldFilePath,'r',encoding='utf8') as file:
-        oldFileData = json.load(file)
-    with open(newFilePath,'r',encoding='utf8') as file:
-        newFileData = json.load(file)
-    return oldFileData,newFileData
-
-#获取接口Json文件中的接口路径
-def getAPIList(data):
-    APIList = []
-    paths = list(data['paths'].keys())
-    for path in paths:
-        apitypelist = list(data['paths'][path].keys())
-        for apitype in apitypelist:
-            api = (apitype.upper(),path)
-            APIList.append(api)
-    return APIList
-
 #对比两个接口列表获取新增数和删除数
-def getAddAndDeleteCount(oldAPIList,newAPIList):
+def getAddAndDeleteCount(prdAPIList,envAPIList):
     addCount = 0 #新增接口数
     deleteCount = 0 #删除接口数
     addList = []
     deleteList = []
-    for oldapi in oldAPIList:
+    envLen = len(envAPIList)
+    prdLen = len(prdAPIList)
+    for oldapi in prdAPIList:
     #删除接口
-        if oldapi not in newAPIList:
+        if oldapi not in envAPIList:
             deleteList.append(oldapi)
             deleteCount += 1
-    for newapi in newAPIList:
-        if newapi not in oldAPIList:
+    for newapi in envAPIList:
+        if newapi not in prdAPIList:
             addList.append(newapi)
             addCount += 1
-    return addCount,addList,deleteCount,deleteList
-
-#字典对比
-def compareDic(dic_old,dic2_new):
-    """对比两个字典的区别，以dic_old为主体进行对比，dic2_new新增key、删除key、value变更"""
-    # dic_old = dict(dic_old)
-    # dic2_new = dict(dic2_new)
-    dict1 = {'a':2,'b':2,'c':3,'d':4}
-    dict2 = {'a':1,'b':2,'c':5,'e':6,'g':4}
-    #新增的key
-    addDictList = []
-    addkey = list(dict2.keys()-dict1.keys())
-    for key in addkey:
-        addDict = {key:dict2[key]}
-        addDictList.append(addDict)
-    print(addDictList)
-    #删除的key
-    delDictList = []
-    delkey = list(dict1.keys()-dict2.keys())
-    for key in delkey:
-        delDict = {key:dict1[key]}
-        delDictList.append(delDict)
-    # 字典值不同
-    allkey = dict1.keys() & dict2.keys()
-    modifyDictList = [({'Key':k, '原值':dict1[k], '当前值':dict2[k]}) for k in allkey if dict1[k] != dict2[k]]
-    diffResult = {'新增key列表':addDictList,'删除key列表':delDictList,'编辑key列表':modifyDictList}
-    return diffResult
-
-#对比接口入参列表
-def compareParametersList(list_old,list_new):
-    list_old =  list(list_old)
-    list_new = list(list_new)
-    oldParameter = []
-    for i in list_old:
-        x = {i['in']:i['name']}
-        oldParameter.append(x)
-    print(oldParameter)
-    newParameter = []
-    for i in list_new:
-        x = {i['in']:i['name']}
-        newParameter.append(x)
-    print(newParameter)
-    addParameList = list(x for x in newParameter if x not in oldParameter)
-    delParameList = list(x for x in oldParameter if x not in newParameter)
-    difflist = {'新增参数':addParameList,'删除参数':delParameList}
-    return difflist
-
-#获取接口编辑对比结果
-def getmodifyresult(oldAPIList,newAPIList,oldFileData,newFileData):
-    modifyCount = 0 #修改接口数
-    modifyContent = {"(1)路径":"","(2)上一版本入参":"","(3)当前版本入参":""}
-    modifyList = []
-    #获取接口编辑信息
-    for api in newAPIList:
-        if api in oldAPIList:
-            if 'parameters' in oldFileData['paths'][api[1]][api[0].lower()].keys():
-                oldapiparameters = oldFileData['paths'][api[1]][api[0].lower()]['parameters']
-                newapiparameters = newFileData['paths'][api[1]][api[0].lower()]['parameters']
-                if oldapiparameters == newapiparameters:
-                    pass
-                else:
-                    print('修改后')
-                    for apiparameter in oldapiparameters:
-                        pass
+    return prdLen,envLen,addCount,addList,deleteCount,deleteList
 
 
+#显示对比结果
+def showCompareResults(service,prdLen,envLen,addCount,addList,deleteCount,deleteList):
+    compare = OrderedDict()
+    compare['服务'] = service
+    compare['当前环境接口总数'] = envLen
+    compare['生产环境接口总数'] = prdLen
+    compare['新增接口数'] = addCount
+    compare['删除接口数'] = deleteCount
+    compare['新增接口详情'] = addList
+    compare['删除接口详情'] = deleteList
+    compareresult = {service:compare}
+    print('服务：'+service.upper()+'\n')
+    print('\t当前环境接口总数：'+str(envLen))
+    print('\t生产环境接口总数：'+str(prdLen))
+    print('\t新增接口数：',end='')
+    lightoutprint(addCount)
+    print('\t删除接口数：',end='')
+    lightoutprint(deleteCount)
+    print('\t新增接口详情：')
+    if addCount:
+        for i in addList:
+            print('\t\t',end='')
+            for key,value in i.items():
+                print(key + ':' + value +'\t',end='')
+            print('\n')
+    else:
+        print('\t\t无')
+    print('\n\t删除接口详情：')
+    if deleteCount:
+        for i in deleteList:
+            print('\t\t',end='')
+            for key,value in i.items():
+                print(key + ':' + value +'\t',end='')
+            print('\n')
+    else:
+        print('\t\t无')
+    print('\n\n')
+    return compare
 
-                    # modifyContent['(1)路径'] = api[0] + api[1]
-                    # modifyContent['(2)上一版本入参'] = oldapiparameters
-                    # modifyContent['(3)当前版本入参'] = newapiparameters
-                    # modifyList.append(modifyContent)
-                    # modifyCount += 1
-    return modifyCount,modifyList
-
-def savecompareresult(oldFilePath,newFilePath,APICompare_Dir,checkresult):
-    difFilename = os.path.basename(oldFilePath)[:-5]+'compare'+os.path.basename(newFilePath)[:-5]+'.json'
-    difFilepath = os.path.join(APICompare_Dir,difFilename)
+def saveAllcompareresult(compareFileDir,compare,isNew=False):
+    compareFilePath = os.path.join(compareFileDir,'AllServicesCompare','.josn')
     #保存对比文件
-    with open(difFilepath,'w',encoding='utf8') as file:
-        json.dump(checkresult,file,ensure_ascii=False,indent=4,sort_keys=True)
-    return difFilepath
+    if isNew:
+        with open(compareFilePath,'w',encoding='utf8') as file:
+            json.dump(compare,file,ensure_ascii=False,indent=4)
+    else:
+        with open(compareFilePath,'a',encoding='utf8') as file:
+            json.dump(compare,file,ensure_ascii=False,indent=4)
+    return compareFilePath
 
 #判断是否高亮输出
 def lightoutprint(count,highlight=0):
     if highlight != 0 or count > 0:
         cmdprintcolor.printYellowRed(str(count)+'\n')
         cmdprintcolor.resetColor()
-        # print("\033[1;31;40m%s\033[0m" %count)
     else:
         print(count)
-
-#显示对比结果
-def showCompareResults(difFilepath,checkresult):
-    addcount = checkresult['3、新增接口数']
-    deltecount = checkresult['4、删除接口数']
-    modifycount = checkresult['5、修改接口数']
-    print("""对比结果保存路径：%s
-1、上一版本接口个数：%s
-2、当前接口个数：%s
-""" %(difFilepath,checkresult['1、上一版本接口个数'],checkresult['2、当前接口个数']),end='')
-    print("3、新增接口数：",end='')
-    lightoutprint(addcount)
-    print('4、删除接口数：',end='')
-    lightoutprint(deltecount)
-    print('5、修改接口数：',end='')
-    lightoutprint(modifycount)
-    print("6、检查结果：")
-    print('\t新增接口详情：')
-    if len(checkresult['6、检查结果']['新增接口详情']):
-        for i in checkresult['6、检查结果']['新增接口详情']:
-            print('\t\t',i[0],'\t',i[1])
-    else:
-        print('\t\t无')
-    print('\n\t删除接口详情：')
-    if len(checkresult['6、检查结果']['删除接口详情']):
-        for i in checkresult['6、检查结果']['删除接口详情']:
-            print('\t\t',i[0],'\t',i[1])
-    else:
-        print('\t\t无')
-    print('\n\t编辑接口详情：')
-    if len(checkresult['6、检查结果']['编辑接口详情']):
-        seq = 1
-        for i in checkresult['6、检查结果']['编辑接口详情']:
-            print('\t\t编辑接口：',seq)
-            print('\t\t\t(1)路径：',i['(1)路径'])
-            print('\t\t\t(2)上一版本入参：',i['(2)上一版本入参'],'\n')
-            print('\t\t\t(3)当前版本入参：',i['(3)当前版本入参'],'\n\n')
-            seq += 1
-    else:
-        print('\t\t无')
-    print('\n\n')
 
 
 
