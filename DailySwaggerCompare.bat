f2933b552dc2860d97e08514a4e8672c7d271596
Zichuan.Yu <Zichuan.Yu@connext.com.cn>
2019-12-10 10:16:16 +0800

    更新bat文件

--- DailySwaggerCompare.bat
+++ DailySwaggerCompare.bat
@@ -1,5 +1,7 @@
-echo DailySwaggerCompare
-cd E:\SoftWare\JetBrains\IntellijCode\PythonCode\SwaggerCompare\SwaggerCompare
+@rem 跳转到bat文件目录
+cd /d %~dp0
+@rem 个人安装了conda，activate mypython36进入Python3环境，可删除
 call activate mypython36
+@rem 执行SwaggerCompare.py，默认对比所有Services
 python SwaggerCompare.py stage 0
 pause
--- SwaggerCompare.py
+++ SwaggerCompare.py
@@ -1,6 +1,3 @@
-import os
-
-import sys
 from myFunction import *
 from Config import *
 
